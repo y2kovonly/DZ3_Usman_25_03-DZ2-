b23b421893e4a7d1803e68a7ba6be13b3a51a9db
y2kovonly <nuridnovm46@gmail.com>
2023-04-15 00:29:56 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const mass=  ['milk', 'beer', 'beer', 'milk', 'milk']
+let answer = ''
+for(1 of mass){
+    if(1=="milk"){
+        answer +=" good"
+    }else{
+        answer +=" beer"
+    }
+}
+console.log(answer)
